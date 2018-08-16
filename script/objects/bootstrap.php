e1f8515ba272fdd37c7cbd6ef86d3b19aa4158a8
Le Trung Kien <kientrungle2001@gmail.com>
2018-08-16 12:00:16 +0700

    commit

--- script/objects/bootstrap.php
+++ script/objects/bootstrap.php
@@ -233,6 +233,14 @@ class PzkBtnLink extends PzkBtn {
 	public $context = 'link';
 }
 
+class PzkBtnCollapse extends PzkBtn {
+	public function init() {
+		$this->{'data-toggle'} = 'collapse';
+		$this->{'aria-expanded'} = 'false';
+		parent::init();
+	}
+}
+
 /* Images */
 class PzkImgResponsive extends PzkHtmlTag {
 	public $tag = 'img';
@@ -451,15 +459,6 @@ class PzkBtnDropdown extends PzkBtn {
 	}
 }
 
-class PzkBtnCollapse extends PzkBtn {
-	public $extendClass = 'navbar-toggle collapsed';
-	public function init() {
-		$this->{'data-toggle'} = 'collapse';
-		$this->{'aria-expanded'} = 'false';
-		parent::init();
-	}
-}
-
 /* Btn Group */
 
 class PzkBtnGroup extends PzkHtmlTag {
@@ -507,7 +506,539 @@ class PzkNavUl extends PzkHtmlTag {
 	}
 }
 
+class PzkNavTabs extends PzkNavUl {
+	public $type = 'tabs';
+}
+
+class PzkNavTabsJustified extends PzkNavTabs {
+	public $align = 'justified';
+}
+
+class PzkNavPills extends PzkNavUl {
+	public $type = 'pills';
+}
+
+class PzkNavPillsJustified extends PzkNavPills {
+	public $align = 'justified';
+}
+
 class PzkNavLi extends PzkHtmlTag {
 	public $tag = 'li';
 	public $role = 'presentation';
+}
+
+class PzkNavItem extends PzkNavLi {
+}
+
+class PzkNavTabsItem extends PzkNavItem {
+}
+
+class PzkNavTabsItemLink extends PzkHtmlTag {
+	public $tag = 'a';
+	public function init() {
+		$attrs = [
+			'role'	=> 'tab',
+			'data-toggle'	=>	'tab'
+		];
+		foreach($attrs as $key => $value) {
+			$this->key = $value;
+		}
+		parent::init();
+	}
+}
+
+/* Navbar */
+class PzkNavbar extends PzkHtmlTag {
+	public $tag = 'nav';
+	public $extendClass = 'navbar';
+	public $type = 'default';
+	public $align = '';
+	public function init() {
+		if($this->type) {
+			$this->extendClass .= ' navbar-' . $this->type;
+		}
+		if($this->align) {
+			$this->extendClass .= ' navbar-' . $this->align;
+		}
+		parent::init();
+	}
+}
+
+class PzkNavbarBtnCollapse extends PzkBtn {
+	public $extendClass = 'navbar-toggle collapsed';
+	public function init() {
+		$this->{'data-toggle'} = 'collapse';
+		$this->{'aria-expanded'} = 'false';
+		parent::init();
+	}
+}
+
+class PzkNavbarHeader extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'navbar-header';
+}
+
+class PzkNavbarBrand extends PzkHtmlTag {
+	public $tag = 'a';
+	public $extendClass = 'navbar-brand';
+}
+
+class PzkNavbarForm extends PzkHtmlTag {
+	public $tag = 'form';
+	public $extendClass = 'navbar-form';
+	public $align = 'left'; // left, right
+	public function init() {
+		if($this->align) {
+			$this->extendClass .= ' navbar-' . $this->align;
+		}
+		parent::init();
+	}
+}
+
+class PzkNavbarNav extends PzkHtmlTag {
+	public $tag = 'ul';
+	public $extendClass = 'nav navbar-nav';
+	public $align = 'right'; // left, right
+	public function init() {
+		if($this->align) {
+			$this->extendClass .= ' navbar-' . $this->align;
+		}
+		parent::init();
+	}
+}
+
+class PzkNavbarNavItem extends PzkHtmlTag {
+	public $tag = 'li';
+}
+
+class PzkNavbarBtn extends PzkBtn {
+	public function init() {
+		$this->extendClass .= ' navbar-btn';
+		parent::init();
+	}
+}
+
+class PzkNavbarText extends PzkHtmlTag {
+	public $tag = 'p';
+	public $extendClass = 'navbar-text';
+}
+
+class PzkNavbarLink extends PzkHtmlTag {
+	public $tag = 'a';
+	public $extendClass = 'navbar-link';
+}
+
+/* Breadcrumbs */
+class PzkBreadcrumb extends PzkHtmlTag {
+	public $tag = 'ol';
+	public $extendClass = 'breadcrumb';
+}
+
+class PzkBreadcrumbItem extends PzkHtmlTag {
+	public $tag = 'li';
+}
+
+/* Pagination */
+
+class PzkPagination extends PzkHtmlTag {
+	public $tag = 'ul';
+	public $extendClass = 'pagination';
+	public $size = '';
+	public function init() {
+		if($this->size) {
+			$this->extendClass .= ' pagination-' . $this->size;
+		}
+		parent::init();
+	}
+}
+
+class PzkPaginationItem extends PzkHtmlTag {
+	public $tag = 'li';
+}
+
+class PzkPager extends PzkHtmlTag {
+	public $tag = 'ul';
+	public $extendClass = 'pager';
+}
+
+class PzkPagerItem extends PzkHtmlTag {
+	public $tag = 'li';
+}
+
+class PzkPagerPrevious extends PzkPagerItem {
+	public $extendClass = 'previous';
+}
+
+class PzkPagerNext extends PzkPagerItem {
+	public $extendClass = 'next';
+}
+
+/* Labels */
+
+class PzkLabelHelper extends PzkHtmlTag {
+	public $tag = 'span';
+	public $extendClass = 'label';
+	public $context = 'default';
+	public function init() {
+		if($this->context) {
+			$this->extendClass .= ' label-' . $this->context;
+		}
+		parent::init();
+	}
+}
+
+class PzkLabelDefault extends PzkLabelHelper {
+	public $context = 'default';
+}
+
+class PzkLabelPrimary extends PzkLabelHelper {
+	public $context = 'primary';
+}
+
+class PzkLabelSuccess extends PzkLabelHelper {
+	public $context = 'success';
+}
+
+class PzkLabelInfo extends PzkLabelHelper {
+	public $context = 'info';
+}
+
+class PzkLabelWarning extends PzkLabelHelper {
+	public $context = 'warning';
+}
+
+class PzkLabelDanger extends PzkLabelHelper {
+	public $context = 'danger';
+}
+
+/* Badge */
+class PzkBadge extends PzkHtmlTag {
+	public $tag = 'span';
+	public $extendClass = 'badge';
+}
+
+/* Jumbotron */
+class PzkJumbotron extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'jumbotron';
+}
+
+/* Page Header */
+class PzkPageHeader extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'page-header';
+}
+
+/* Thumbnail */
+class PzkThumbnailLink extends PzkHtmlTag {
+	public $tag = 'a';
+	public $extendClass = 'thumbnail';
+}
+class PzkThumbnail extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'thumbnail';
+}
+
+class PzkThumbnailImg extends PzkHtmlTag {
+	public $tag = 'img';
+	public $extendClass = 'img-responsive';
+}
+
+class PzkThumbnailCaption extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'caption';
+}
+
+/* Alerts */
+class PzkAlertHelper extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'alert';
+	public $context = 'default';
+	public $role = 'alert';
+	public function init() {
+		if($this->context) {
+			$this->extendClass .= ' alert-' . $this->context;
+		}
+		parent::init();
+	}
+}
+
+class PzkAlertDefault extends PzkAlertHelper {
+	public $context = 'default';
+}
+
+class PzkAlertPrimary extends PzkAlertHelper {
+	public $context = 'primary';
+}
+
+class PzkAlertSuccess extends PzkAlertHelper {
+	public $context = 'success';
+}
+
+class PzkAlertInfo extends PzkAlertHelper {
+	public $context = 'info';
+}
+
+class PzkAlertWarning extends PzkAlertHelper {
+	public $context = 'warning';
+}
+
+class PzkAlertDanger extends PzkAlertHelper {
+	public $context = 'danger';
+}
+
+/* Progressbar */
+
+class PzkProgress extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'progress';
+}
+
+class PzkProgressBar extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'progress-bar';
+	public $role = 'progressbar';
+	// field aria-valuenow
+	// field aria-valuemin
+	// field aria-valuemax
+	public $value = '60';
+	public $context = '';
+	public $type = ''; // striped
+	public function init () {
+		$this->style = 'width: '. $this->value . '%';
+		if($this->context) {
+			$this->extendClass .= ' progress-bar-' . $this->context;
+		}
+		if($this->type) {
+			$this->extendClass .= ' progress-bar-' . $this->type;
+		}
+		parent::init();
+	}
+}
+
+/* Media Objects */
+
+class PzkMedia extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'media';
+}
+
+class PzkMediaLeft extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'media-left';
+}
+
+class PzkMediaRight extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'media-right';
+}
+
+class PzkMediaObject extends PzkHtmlTag {
+	public $tag = 'img';
+	public $extendClass = 'media-object';
+}
+
+class PzkMediaBody extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'media-body';
+}
+
+class PzkMediaBodyHeading extends PzkHtmlTag {
+	public $tag = 'h4';
+	public $extendClass = 'media-heading';
+}
+
+class PzkMediaList extends PzkHtmlTag {
+	public $tag = 'ul';
+	public $extendClass = 'media-list';
+}
+
+class PzkMediaListItem extends PzkMedia {
+	public $tag = 'li';
+}
+
+/* List Group */
+class PzkListGroup extends PzkHtmlTag {
+	public $tag = 'ul';
+	public $extendClass = 'list-group';
+}
+
+class PzkListGroupItem extends PzkHtmlTag {
+	public $tag = 'li';
+	public $extendClass = 'list-group-item';
+	public $context = '';
+	public function init() {
+		if($this->context) {
+			$this->extendClass .= ' list-group-item-' . $this->context;
+		}
+		parent::init();
+	}
+}
+
+class PzkListGroupDiv extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'list-group';
+}
+
+class PzkListGroupItemLink extends PzkListGroupItem {
+	public $tag = 'a';
+}
+
+class PzkListGroupItemBtn extends PzkListGroupItem {
+	public $tag = 'button';
+	public $type = 'button';
+}
+
+class PzkListGroupItemHeading extends PzkHtmlTag {
+	public $tag = 'h4';
+	public $extendClass = 'list-group-item-heading';
+}
+
+class PzkListGroupItemText extends PzkHtmlTag {
+	public $tag = 'p';
+	public $extendClass = 'list-group-item-text';
+}
+
+/* Panels */
+class PzkPanel extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'panel';
+	public $context = 'default';
+	public function init() {
+		if($this->context) {
+			$this->extendClass .= ' panel-' . $this->context;
+		}
+		parent::init();
+	}
+}
+
+class PzkPanelHeading extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'panel-heading';
+}
+
+class PzkPanelBody extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'panel-body';
+}
+
+class PzkPanelFooter extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'panel-footer';
+}
+
+class PzkWell extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'well';
+	public $size = '';
+	public function init() {
+		if($this->size) {
+			$this->extendClass .= ' well-' . $this->size;
+		}
+		parent::init();
+	}
+}
+
+class PzkEmbedResponsive extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'embed-responsive';
+	public $size = '';
+	public function init() {
+		if($this->size) {
+			$this->extendClass .= ' embed-responsive-' . $this->size;
+		}
+		parent::init();
+	}
+}
+
+class PzkEmbedResponsiveItem extends PzkHtmlTag {
+	public $tag = 'iframe';
+	public $extendClass = 'embed-responsive-item';
+}
+
+/* Modal */
+
+class PzkModal extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'modal fade';
+	public $tabindex = '-1';
+	public $role = 'dialog';
+}
+
+class PzkModalDialog extends PzkHtmlTag {
+	public $tag = 'div';
+	public $role = 'document';
+	public $extendClass = 'modal-dialog';
+	public $size = ''; // lg, sm
+	public function init() {
+		if($this->size) {
+			$this->extendClass .= ' modal-' . $this->size;
+		}
+		parent::init();
+	}
+}
+
+class PzkModalContent extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'modal-content';
+}
+
+class PzkModalHeader extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'modal-header';
+}
+
+class PzkModalTitle extends PzkHtmlTag {
+	public $tag = 'h4';
+	public $extendClass = 'modal-title';
+}
+
+class PzkModalBody extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'modal-body';
+}
+
+class PzkModalFooter extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'modal-footer';
+}
+
+/* Tabs */
+class PzkTabContent extends PzkHtmlTag {
+	public $tag = 'div';
+	public $extendClass = 'tab-content';
+}
+
+class PzkTabPane extends PzkHtmlTag {
+	public $tag = 'div';
+	public $role = 'tabpanel';
+	public $extendClass = 'tab-pane fade';
+}
+
+/* Tooltip */
+class PzkBtnTooltip extends PzkBtn {
+	public $align = '';
+	public $title = 'tooltip';
+	public function init() {
+		$this->{'data-toggle'} = 'tooltip';
+		if($this->align) {
+			$this->{'data-placement'} = $this->align;
+		}
+		parent::init();
+	}
+}
+
+/* Popover */
+
+class PzkBtnPopover extends PzkBtn {
+	public $align = '';
+	public $title = 'tooltip';
+	public function init() {
+		$this->{'data-toggle'} = 'popover';
+		$this->{'data-container'} = 'body';
+		if($this->align) {
+			$this->{'data-placement'} = $this->align;
+		}
+		parent::init();
+	}
 }
